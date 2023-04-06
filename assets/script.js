a792fb5f641b9a31a277b31d5c6eccfd7b5a0b3d
Jimmys Mac <eadesj25@gmail.com>
2023-04-06 07:59:20 -0400

    api logic works for today's weather

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
-var APIKey = '9a7846c4d42fa23a212917672d587d16';
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+// var APIKey = '9a7846c4d42fa23a212917672d587d16';
+var searchForm = document.querySelector('#search-form');
+var input = document.querySelector('#input');
+var recentSearches = document.querySelector('.recent');
+var weatherToday = document.querySelector('#weather-today');
+var forecast = document.querySelector('#forecast');
 
 var city;
 // HOW TO FORMAT FETCH REQUEST:
@@ -15,10 +18,57 @@ var city;
 //     console.log(data);
 //   })
 
-fetch(apiUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var city = input.value.trim();
+
+  if (city) {
+    getWeather(city);
+
+    var li = document.createElement('li');
+    li.textContent = city;
+    recentSearches.appendChild(li);
+
+    input.value = '';
+  } else {
+    alert('Please enter a city name')
+  }
+}
+
+var getWeather = function (city) {
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9a7846c4d42fa23a212917672d587d16"
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to today\'s weather');
+    });
+}
+
+function displayWeather(weatherData) {
+  // Update Today's weather section
+  var todayWeatherHtml = '';
+  todayWeatherHtml += '<div>City: ' + weatherData.name + '</div>';
+  todayWeatherHtml += '<div>Temperature: ' + weatherData.main.temp + '</div>';
+  todayWeatherHtml += '<div>Description: ' + weatherData.weather[0].description + '</div>';
+  todayWeatherHtml += '<div>Humidity: ' + weatherData.main.humidity + '</div>';
+  todayWeatherHtml += '<div>Wind Speed: ' + weatherData.wind.speed + '</div>';
+  weatherToday.innerHTML = todayWeatherHtml;
+
+  // Update 5-day Forecast section
+  var forecastHtml = '';
+  // Add your code here to generate the HTML for the forecast section
+  forecast.innerHTML = forecastHtml;
+}
+
+
+searchForm.addEventListener('submit', formSubmitHandler);
