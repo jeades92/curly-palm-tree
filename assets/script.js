737b161efac42c95da7819e4219333b790225a43
Jimmys Mac <eadesj25@gmail.com>
2023-04-09 15:24:17 -0400

    added styling

--- assets/script.js
+++ assets/script.js
@@ -37,7 +37,7 @@ var formSubmitHandler = function (event) {
 }
 
 var getWeather = function (city) {
-  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9a7846c4d42fa23a212917672d587d16"
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=9a7846c4d42fa23a212917672d587d16"
 
   fetch(apiUrl)
     .then(function (response) {
@@ -50,25 +50,44 @@ var getWeather = function (city) {
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to today\'s weather');
+      alert('Unable to connect to weather forecast');
     });
 }
 
 function displayWeather(weatherData) {
   // Update Today's weather section
   var todayWeatherHtml = '';
-  todayWeatherHtml += '<div>City: ' + weatherData.name + '</div>';
-  todayWeatherHtml += '<div>Temperature: ' + weatherData.main.temp + '</div>';
-  todayWeatherHtml += '<div>Description: ' + weatherData.weather[0].description + '</div>';
-  todayWeatherHtml += '<div>Humidity: ' + weatherData.main.humidity + '</div>';
-  todayWeatherHtml += '<div>Wind Speed: ' + weatherData.wind.speed + '</div>';
+  todayWeatherHtml += '<div>City: ' + weatherData.city.name + '</div>';
+
+  // Convert the temperature value from Kelvin to Fahrenheit
+  var tempF = (weatherData.list[0].main.temp - 273.15) * 1.8 + 32;
+
+  todayWeatherHtml += '<div>Temperature: ' + tempF.toFixed(2) + '&deg;F</div>'; // Display the temperature in Fahrenheit
+  todayWeatherHtml += '<div>Description: ' + weatherData.list[0].weather[0].description + '</div>';
+  todayWeatherHtml += '<div>Humidity: ' + weatherData.list[0].main.humidity + '</div>';
+  todayWeatherHtml += '<div>Wind Speed: ' + weatherData.list[0].wind.speed + '</div>';
   weatherToday.innerHTML = todayWeatherHtml;
 
   // Update 5-day Forecast section
   var forecastHtml = '';
-  // Add your code here to generate the HTML for the forecast section
+  for (var i = 0; i < weatherData.list.length; i++) {
+    if (weatherData.list[i].dt_txt.indexOf('15:00:00') !== -1) {
+
+      // Convert the temperature value from Kelvin to Fahrenheit
+      var tempF = (weatherData.list[i].main.temp - 273.15) * 1.8 + 32;
+
+      forecastHtml += '<div class="col-md-2 card bg-primary text-white p-2">';
+      forecastHtml += '<h5>' + new Date(weatherData.list[i].dt_txt).toLocaleDateString() + '</h5>';
+      forecastHtml += '<h6>' + weatherData.list[i].weather[0].description + '</h6>';
+      forecastHtml += '<div>Temp: ' + tempF.toFixed(2) + '&deg;F</div>'; // Display the temperature in Fahrenheit
+      forecastHtml += '<div>Humidity: ' + weatherData.list[i].main.humidity + '%</div>';
+      forecastHtml += '<div>Wind: ' + weatherData.list[i].wind.speed + ' MPH</div>';
+      forecastHtml += '</div>';
+    }
+  }
   forecast.innerHTML = forecastHtml;
 }
+
 
 
 searchForm.addEventListener('submit', formSubmitHandler);
